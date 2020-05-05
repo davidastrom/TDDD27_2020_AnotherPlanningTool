91e963dfaf54e262043502964d4fc6cbd68d6ebc
David Åström <davas593@student.liu.se>
2020-05-05 10:40:08 +0200

    Added gitlab-ci support First iteration, only build support

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('AnotherPlanningTool');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AnotherPlanningTool app is running!');
-  });
 });
