cefc94d0e85668125d19e61e7cd089b3d0d412a3
David Åström <davas593@student.liu.se>
2020-06-02 13:56:50 +0200

    Configured working route guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileListComponent } from './components/profile-list/profile-list.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
-	{ path: '', component: ProfileListComponent },
+	{ path: '', component: ProfileListComponent, canActivate: [AuthGuard] },
 	{ path: '**', redirectTo: '' },
 ];
 
--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { catchError, retry, map } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { GetCurrentUserGQL, User } from '../services/graphql.service';
 
 @Injectable({
 	providedIn: 'root',
@@ -10,9 +11,30 @@ import { Router } from '@angular/router';
 export class AuthService {
 	googleLoginUrl = '/auth/google';
 
-	constructor(private http: HttpClient, private router: Router) {}
+	redirectUrl: string;
+
+	constructor(
+		private http: HttpClient,
+		private router: Router,
+		private currentUserService: GetCurrentUserGQL
+	) {}
 
 	googleLogin() {
 		window.location.href = this.googleLoginUrl;
+	}
+
+	getCurrentUser() {
+		return this.currentUserService
+			.fetch()
+			.pipe(map((res) => res.data.currentUser));
+	}
+
+	isLoggedIn(): Observable<boolean> {
+		return this.getCurrentUser().pipe(
+			map((user) => {
+				if (user == null) return false;
+				return true;
+			})
+		);
 	}
 }
