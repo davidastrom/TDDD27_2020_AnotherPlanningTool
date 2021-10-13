089a6527878bbd9cc69e00d800ef4f2415d6d897
David Åström <davas593@student.liu.se>
2021-10-13 22:05:00 +0200

    Attempt at fixing docker issues. Attempts finally aborted

--- client/src/environments/environment.docker.ts
+++ client/src/environments/environment.docker.ts
@@ -6,5 +6,6 @@ export const environment = {
 				'179758113572-mn77iu9u85g5r24loi9br6epq92i9k66.apps.googleusercontent.com',
 		},
 	},
-	apiUrl: 'http://express-server:8080',
+	apiUrl: 'https://localhost:8080',
+	apiWsUrl: 'wss://localhost:8080',
 };
